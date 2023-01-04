d8fb01bae4128b87b80e3bb3248fc20973972a6d
DangAnh982001 <danganh982001@gmail.com>
2023-01-04 08:05:58 +0700

    First_commit_DTA037

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DangThiAnh037.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
